e20c7932e22db5c5f7ee82d5bafe9cffa2ceca78
luuthinh <luuthinh2705@gmail.com>
2020-07-08 22:38:28 +0700

    Many2one

--- src/component/Many2one.js
+++ src/component/Many2one.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {View, TouchableOpacity, Dimensions, StyleSheet, FlatList} from 'react-native';
-import { Portal, Dialog, Text, Button, Divider, Searchbar, Paragraph} from 'react-native-paper';
+import { Portal, Dialog, Text, Button, Checkbox, Searchbar, Paragraph} from 'react-native-paper';
 import PropsTypes from 'prop-types';
 
 const {width, height} = Dimensions.get('window');
@@ -22,13 +22,19 @@ class Many2one extends React.Component {
             preSelectedItem: {},
             selectedItem: {},
             data: [
-                {id:1, name:"Thịnh"}
             ],
             keyword: ''
         }
     }
-    cancelSelection = () => {
-
+    _cancelSelection = () => {
+        let {data, preSelectedItem} = this.state;
+        data.map(item =>{
+            item.checked = false;
+            if (item.id === preSelectedItem.id){
+                item.checked = true
+            }
+        })
+        this.setState({data, visible:false, keyword:'', selectedItem: preSelectedItem})
     }
     _onchangeSearch = (keyword) => {
         this.setState({keyword: keyword})
@@ -64,24 +70,53 @@ class Many2one extends React.Component {
     componentDidUpdate(prevProps){
     }
     showDialog = () => this.setState({'visible':true})
-    hideDialog = () => this.setState({'visible': false})
 
-	_keyExtractor = item => {
-		return item.id.toString();
-	};
-    _renderItem = data => {
+	_keyExtractor = idx => {
+		return idx.id.toString();
+    };
+    
+    _onItemSelected = (item) => {
+        let selectedItem = [];
+        let {data} = this.state;
+        item.checked = !item.checked;
+        for (let index in data){
+            if (data[index].id === item.id){
+                data[index] = item
+            }
+            else {
+                data[index].checked = false
+            }
+            
+        }
+        if (item.checked) selectedItem=item
+        this.setState({data, selectedItem})
+    }
+
+    _renderItem = (data,idx) => {
         return (
-            <Text>{data.item.name}</Text>
+            <TouchableOpacity
+                key={idx}
+                onPress={() => this._onItemSelected(data.item)}
+                activeOpacity={0.7}
+                style={styles.itemWrapper}
+            >
+                <Text>{data.item.name}</Text>
+                <Checkbox.Item
+                    style={styles.itemIcon} 
+                    status={data.item.checked ? 'checked':'unchecked'}
+                />
+            </TouchableOpacity>
         );
      };    
 
     render(){
         let {cancelButtonText,selectButtonText} = this.props
         let { visible, selectedItem, preSelectedItem} = this.state
+        console.log(preSelectedItem)
         return(
             <TouchableOpacity onPress={this.showDialog}>
                 {
-                    preSelectedItem.length > 0 
+                    preSelectedItem
                         ? (
                             <View>
                                 <Text>{preSelectedItem.name}</Text>
@@ -109,8 +144,22 @@ class Many2one extends React.Component {
                         />
                         </Dialog.ScrollArea>
                         <Dialog.Actions>
-                            <Button onPress={this.hideDialog}>{selectButtonText}</Button>
-                            <Button onPress={this.hideDialog}>{cancelButtonText}</Button>
+                            <Button 
+                                onPress={this._cancelSelection}
+                                mode='outlined'
+                                style={styles.button}
+                            >
+                                {cancelButtonText}
+                            </Button>
+                            <Button 
+                                onPress={()=>{
+                                    this.setState({visible:false,keyword:'',preSelectedItem:selectedItem})
+                                }}
+                                mode='contained'
+                                style={[styles.button,]}
+                            >
+                                <Text style={{color:'white'}}>{selectButtonText}</Text>
+                            </Button>                            
                         </Dialog.Actions>
                     </Dialog>
                 </Portal>
@@ -129,7 +178,46 @@ const styles = StyleSheet.create({
         borderTopLeftRadius: 8, 
         borderTopRightRadius: 8,
         height: INIT_HEIGHT,
-    }
+    },
+    title: {
+        fontSize: 16, marginBottom: 16, width: '100%', textAlign: 'center'
+    },
+    line: {
+        height: 1, width: '100%', backgroundColor: '#cacaca'
+    },
+    inputKeyword: {
+        height: 40, borderRadius: 5, borderWidth: 1, borderColor: '#cacaca',
+        paddingLeft: 8, marginHorizontal: 24, marginTop: 16
+    },
+    buttonWrapper: {
+        marginVertical: 16, flexDirection: 'row', alignItems: 'center', justifyContent: 'center'
+    },
+    button: {
+        height: 36, flex: 1
+    },
+    selectedTitlte: {
+        fontSize: 14, color: 'gray', flex: 1
+    },
+    tagWrapper: {
+        flexDirection: 'row', flexWrap: 'wrap'
+    },
+    listOption: {
+        paddingHorizontal: 24,
+        paddingTop: 1, marginTop: 16
+    },
+    itemWrapper: {
+        borderBottomWidth: 1, borderBottomColor: '#eaeaea',
+        paddingVertical: 12, flexDirection: 'row', alignItems: 'center'
+    },
+    itemText: {
+        fontSize: 16, color: '#333', flex: 1
+    },
+    itemIcon: {
+        width: 30, textAlign: 'right'
+    },
+    empty: {
+        fontSize: 16, color: 'gray', alignSelf: 'center', textAlign: 'center', paddingTop: 16
+    }    
 })
 
 Many2one.propsTypes = {
